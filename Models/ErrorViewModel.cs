f4c488f31da0fe62f3fc6e55f05c95353bf68901
Jessica Ejelöv <jessicaejelov@gmail.com>
2023-02-13 20:28:29 +0100

    init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace music_collection.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
