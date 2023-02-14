a2c61f3e752b98000f73b542947fdd69bf5f4134
Jessica Ejelöv <jessicaejelov@gmail.com>
2023-02-14 14:04:50 +0100

    added context file

---
+++ Data/MusicContext.cs
@@ -0,0 +1,16 @@
+using music_collection.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace music_collection.Data
+{
+    public class MusicContext : DbContext
+    {
+        public MusicContext(DbContextOptions<MusicContext> options) : base(options)
+        {
+            //constructor
+        }
+
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Album> Albums { get; set; }
+    }
+}
